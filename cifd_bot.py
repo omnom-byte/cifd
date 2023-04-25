0ca671ab4b08f2d3a0b6936fc8f001f11dec5c72
omnom-byte <131674821+omnom-byte@users.noreply.github.com>
2023-04-25 21:14:13 +0200

    Update cifd_bot.py

--- cifd_bot.py
+++ cifd_bot.py
@@ -30,6 +30,43 @@ class Bot:
             self.df = pd.DataFrame()
         self.bot_start_time = time.time()
 
+     def backtest(self):
+        data = read_data("data.json")
+        self.signals.reset()
+        for i in range(len(data)):
+            signal = Signal()
+            signal.symbol = self.symbol
+            signal.price = data[i]["close"]
+            signal.time = data[i]["time"]
+            if self.signals.last_signal() is not None:
+                self.signals.last_signal().close_time = signal.time
+            check_signal = signal.copy()
+
+            if self.strategy == "rsi":
+                check_signal.data = (data[:i], self.rsi_period, self.rsi_upper, self.rsi_lower)
+                check_signal.conditions.append(self.strategies["rsi"]["enter"])
+                check_signal.conditions.append(self.strategies["rsi"]["exit"])
+            elif self.strategy == "macd":
+                check_signal.data = (data[:i], self.macd_fast, self.macd_slow, self.macd_signal)
+                check_signal.conditions.append(self.strategies["macd"]["enter"])
+                check_signal.conditions.append(self.strategies["macd"]["exit"])
+            elif self.strategy == "bb":
+                check_signal.data = (data[:i], self.bb_period, self.bb_dev_factor)
+                check_signal.conditions.append(self.strategies["bb"]["enter"])
+                check_signal.conditions.append(self.strategies["bb"]["exit"])
+            self.signals.add_signal(signal)
+            if not self.signals.check_conditions(check_signal):
+                self.signals.remove_last_signal()
+
+        self.write_to_sheet()
+        return self.signals.summary()
+      
+    def write_to_sheet(self):
+        if self.sheet_id:
+            values = [
+                [datetime.now().strftime("%d/%m/%Y %H:%M:%S"), self.get_balance(), self.run_count, self.get_trades_count(),
+                 self.get_profit_loss(), self.get_profit_loss_rate(), self.signals.all_signals_count(),
+    
     def run(self):
         logging.info('Bot started at {}'.format(datetime.now(pytz.timezone(config.TIMEZONE)).strftime('%Y-%m-%d %H:%M:%S')))
         while True:
@@ -69,17 +106,17 @@ class Bot:
                         self.place_order(symbol, 'BUY', latest_price)
                     elif rsi_value <= config.RSI_OVERSOLD and macd_value > macd_signal and latest_price >= bb_upper:
                         self.place_order(symbol, 'SELL', latest_price)
-            time.sleep(self.interval)
-            
-    def check_balance(self):
-        balances = self.exchange.get_balances()
-        self.balance = float(balances['BTC']['free'])
-        print("Your current balance is: ", self.balance)
+            time.sleep(self.interval)           
 
     def get_wallet(self):
         wallet = self.exchange.get_wallet()
         self.wallet = wallet
         return wallet
+      
+    def check_balance(self):
+    balances = self.exchange.get_balances()
+    self.balance = float(balances['BTC']['free'])
+    print("Your current balance is: ", self.balance)
 
     def place_order(self, symbol, order_type, price):
         if order_type == 'BUY':
@@ -158,3 +195,5 @@ class Bot:
         chart.execute()
 
         logging.info('Chart added to Google Sheets: {}'.format(sheet_name))
+
+   
